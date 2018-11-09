b1290167b702329e36bcc78b325506b3e247b7ad
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-11-09 08:14:04 -0500

    almost finished alpha

---
+++ src/Hangman.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main (String[]args){
+
+     String story;
+     String guess="chocolate factory";
+     int points =1;
+        Scanner mac=new Scanner(System.in);
+
+     System.out.println("We are going to play a game of Hangman.Try your best to guess the correct words" +
+             " ,you have 5 lives.Before we start make a backstory for your Hangman");
+
+        System.out.println("BACKSTORY");
+        story= mac.nextLine();
+
+
+for(int damage=1;damage<6;damage++){
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
