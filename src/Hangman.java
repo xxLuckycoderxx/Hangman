d4005a3e1a6eb7f935932f28d0f862031a619cf0
xxLuckycoderxx <42833597+xxLuckycoderxx@users.noreply.github.com>
2018-11-12 19:15:40 -0500

    Update Hangman.java
    made some additions to the code

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,26 +2,34 @@ import java.util.Scanner;
 
 public class Hangman {
     public static void main (String[]args){
-
+        
+     String extraInfo;   
+     String name;
      String story;
-     String guess="chocolate factory";
-     int points =1;
+     String guess="school";
+       
+     int points =6;
         Scanner mac=new Scanner(System.in);
-
-     System.out.println("We are going to play a game of Hangman.Try your best to guess the correct words" +
-             " ,you have 5 lives.Before we start make a backstory for your Hangman");
+        
+    System.out.println("Hello my name is Mac and we are going to play a game.Before that ,what is your name?");    
+      name=mac.nextLine();
+     System.out.println("Well " + name+ " we are going to play a game of Hangman.Try your best to guess the correct words" +
+             " ,You have 6 lives.Before we start make a backstory for your Hangman");
 
         System.out.println("BACKSTORY");
         story= mac.nextLine();
+     System.outprintln("Anything else you want to write about about your character?Include it below")
+     extraInfo.nextLine();
 
-
-
-
-
-
-
-
-}
+for(int net=1;net<7;net++){
+   
+ if(           ){
+     System.out.println(point-1+" tries left");
+ }
+     
+    
+} 
+ 
 
 
 
@@ -36,5 +44,6 @@ public class Hangman {
 
 
     }
+    public static void(
 }
 
