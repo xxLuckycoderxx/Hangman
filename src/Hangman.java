c50ad37e59fe55117adbcc77fe41762f95ec0767
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-11-26 08:01:22 -0500

    almost finished with beta

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Hangman {
-    public static void main (String[]args){
+    public static void main(String[] args) {
         
     /* String extraInfo;
      String name;
@@ -25,36 +25,45 @@ for(int net=1;net<7;net++){
    
  if(){
      System.out.println(point-1+" tries left");
- }
-     
-    
+
+
+    }
+
 } 
  
 */
 
+ System.out.println("Would you like to give up if so type (0)in the space below ");
+String exit=mac.nextLine();
+ int quit=0;
+ if(exit== 0) {
+     System.exit(0);
 
+  System.out.println("Now that you have gotten used to the  game why dont you choose a gender for your hangperson\n type either male or female ");
+  String gender=mac.nextline();
 
-
-
-
-
-
+  System.out.println("So how are you liking the game so far from a scale of 1-10 ");
+  int rating=mac.nextInt();
+ }
     }
+
     public static void hangedMan() {
 
-        /*System.out.println("+----------------------------------");
+        /*System.out.println("+---------------------------------");
         System.out.println("|                |                  ");
         System.out.println("|                |                  ");
         System.out.println("|                O                  ");
         System.out.println("|         ----   1 ------           ");
         System.out.println("|                |                  ");
-        System.out.println("|                |                  ");
+        System.out.println("|               _|_                 ");
         System.out.println("|              /    \               ");
-        System.out.println("|             /       \             ");
-        System.out.println("|           _/        \_          ");
+        System.out.println("|             /      \              ");
+        System.out.println("|           _/        \_            ");
         System.out.println("|                                   ");
         System.out.println("+---------------------------------- ");
 */
     }
+   public static int pharmacy(int a,int b){
+        return a+b;
+   }
 }
-
