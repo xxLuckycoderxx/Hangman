c65efd3e0e9f19bb5d44847d9c2e737925599c9a
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-12-13 08:57:22 -0500

    change made

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
-/*
+
         String extraInfo;
         String name;
         String story;
         String guess = "school";
-        String input;
+        String word;
         int point = 6;
         Scanner mac = new Scanner(System.in);
 
@@ -21,67 +21,61 @@ public class Hangman {
         System.out.println("Anything else you want to write about about your character?Include it below");
         extraInfo = mac.nextLine();
 
+        System.out.println("Now that you have gotten used to the  game why don't you choose a gender for your hangperson\n type either male or female ");
+        String gender = mac.nextLine();
+
+        String words = "school";
+        System.out.println("So how are you liking the game so far from a scale of 1-10 ");
+        int rating = mac.nextInt();
+        System.out.println("Thank you for your time");
+
+
+
+        int quit = 0;
+        System.out.println("Would you like to give up if so type (0)in the space below \n Exit:");
+        String exit = mac.nextLine();
+        if (exit.equals(quit)) {
+            System.exit(0);
+
+
         for (int net = 1; net < 7; net++) {
+            display();
 
-            if (input.equals(4)) {
-                System.out.println(point - 1 + " tries left");
+            System.out.println("Give me a guess/n Guess:");
+            String input = mac.nextLine();
+
+            if (input.equals(words)) {
+                winner();
+                System.out.println("WINNER!!");
+            } else {
+                deadMan();
+                System.out.println("LOSSER");
+            }
+
+                System.out.println("You have completed the game so do you have any suggestions on how to improve it ?\nSuggestion:");
+                String suggestions = mac.nextLine();
+                System.out.println("Thank you for your input");
+
+                System.out.println("Give me some commentary \n Commentary");
+                String comments = mac.nextLine();
+                System.out.println("Thank you for your input");
+
+
+
+
+
+
+
+
+
 
             }
 
 
-            System.out.println("You have completed the game so do you have any suggestions on how to improve it ?\nSuggestion;");
-            String suggestions = mac.nextLine();
-            System.out.println("Yhank you for your input");
-
-            System.out.println("Give me some commentary \n Commentary");
-            String comments = mac.nextLine();
-            System.out.println("Thank you for your input");
-
-
-            System.out.println("Would you like to give up if so type (0)in the space below ");
-            String exit = mac.nextLine();
-            int quit = 0;
-            if (exit.equals(quit)) {
-                System.exit(0);
-
-                System.out.println("Now that you have gotten used to the  game why don't you choose a gender for your hangperson\n type either male or female ");
-                String gender = mac.nextLine();
-
-                System.out.println("So how are you liking the game so far from a scale of 1-10 ");
-                int rating = mac.nextInt();
-
-                System.out.println("Thank you for your time");
-
-                System.out.println("Thank you for your time");
-
-
-            }
-
-            static String hangedMan (){
 
 
 
-
-                System.out.println("+--------------------------------- ");
-                System.out.println("|                |                 ");
-                System.out.println("|                |                 ");
-                System.out.println("|                O                 ");
-                System.out.println("|           ---- 1------           ");
-                System.out.println("|                |                 ");
-                System.out.println("|               _|_                ");
-                System.out.println("|              |   |              ");
-                System.out.println("|              |   |              ");
-                System.out.println("|             _|   |_             ");
-                System.out.println("|                                  ");
-                System.out.println("+----------------------------------");
-
-
-            }}*/
-        Scanner keyboard=new Scanner(System.in);
-        display();
-       System.out.println("give me words");
-       String input=keyboard.nextLine();
-
+      }
     }
   static void display(){
 
@@ -99,5 +93,31 @@ public class Hangman {
       System.out.println("+----------------------------------+");
 
      }
-}
+     static void deadMan(){
+        System.out.println("+________________ + ");
+        System.out.println("|        O          |");
+        System.out.println("|        |          |");
+        System.out.println("|       -|-         |");
+        System.out.println("|       / /         |");
+        System.out.println("|        |          |");
+        System.out.println("|       _|_         |");
+        System.out.println("|      /  /         |");
+        System.out.println("|     /   /         |");
+        System.out.println("+________________  +");
+     }
+     static void winner(){
+
+     System.out.println("+--------------------------------- +");
+      System.out.println("|                |                 |");
+      System.out.println("|              | |  /               |");
+      System.out.println("|              | O /                |");
+      System.out.println("|              |_1/                |");
+      System.out.println("|                |                 |");
+      System.out.println("|               _|_                |");
+      System.out.println("|              |   |               |");
+      System.out.println("|              |   |               |");
+      System.out.println("|             _|   |_              |");
+      System.out.println("|                                  |");
+      System.out.println("+----------------------------------+");
+}}
 
